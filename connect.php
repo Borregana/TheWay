12f3085d1a3b27cd1e3218ae2869e75670430795
Pablo Salas <Borregana@gmail.com>
2014-08-27 17:40:48 +0200

    first fail

--- connect.php
+++ connect.php
@@ -6,9 +6,7 @@
  * Time: 17.31
  */
 session_start();
-if(isset($_SESSION['alias'])){
     $con=mysqli_connect("localhost","root","root","Rutas");
     if(mysqli_connect_errno()){
         echo "No se pudo conectar con la base de datos".mysqli_connect_error();
     }
-}
